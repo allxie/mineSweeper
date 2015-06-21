88b20faf93f46f5b1b812bbffd57f8529909460e
Allxie <allxiecleary@gmail.com>
2015-06-20 20:53:15 -0700

    function exists to add board of flexible dimensions. Styled board

---
+++ minesweeper.js
@@ -0,0 +1,47 @@
+//Take input about dimensions
+
+//depending on dimensions, append squares.
+//Line break when rows get to a certain size
+//
+
+//Array of objects
+// {"number" : x
+// ,"value" : 0-8 or mine
+// ,""}
+
+//JSON suggestion:
+// Array of objects
+// {"x-axis": x,
+// "y-axis" : y,
+// "visible": true/false,
+// "minePresence" : true/false,
+// "displayValue" : 0-9}
+//
+// (0 is mine, 9 is blank, or vice versa)
+
+//when we click a square, it changes color::
+window.onload =function(){
+	var thisRow;
+
+	for(i=0; i<20; i++){
+		thisRow = "";
+		console.log("doing the thing");
+		for (var j = 0; j < 20; j++) {
+			thisRow  += "<td id='s61' class='box'></td>";
+			// $("#board").append( "<td id='s61' class='box'></td>" );
+		};
+		$("#board").append( "<tr>" + thisRow + "</tr>" );
+		// $("#board").append( "Goodbye</tr>" );
+	};
+	var $square = $('#board');
+	console.log("page loaded!");
+
+	var sweep = function(){
+		console.log("WASSAP");
+		var $x = $(event.target);
+		$x.css("background-color","red");
+	}
+
+	$($square).click(sweep);
+
+}
