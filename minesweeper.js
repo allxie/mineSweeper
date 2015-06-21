aa3d9e30cb8c4b7077c30094f7b04a2aeafc8454
Allxie <allxiecleary@gmail.com>
2015-06-20 21:37:36 -0700

    add error handling for form

--- minesweeper.js
+++ minesweeper.js
@@ -28,24 +28,40 @@ window.onload =function(){
 		var thisRow;
 		// grabs the two dimensions or defaults to 20
 		var boardHeight = $("#boardHeight").val() || 20;
-		var boardWidth = $("#boardHWidth").val() || 20;
-		var numMines = $("numMines");
+		var boardWidth = $("#boardWidth").val() || 20;
+		var numMines = $("#numMines").val();
 		var squareCount = 0;
-		// Cycles through each for the board height
-		for(i=0; i <= boardHeight; i++){
-			thisRow = "";
-			for (var j = 0; j <= boardWidth; j++) {
-				// builds up a row with the correct width
-				thisRow  += "<td id='s" + squareCount + "' class='box'></td>";
-				squareCount += 1;
+		$("#boardHeight").val("");
+		$("#boardWidth").val("");
+		$("#numMines").val("");
+
+		var totalSquares = boardHeight * boardWidth;
+
+		//if the user put more mines than there are squares
+		if(totalSquares < numMines){
+			$("#message").html("Dude. Too many mines. You definitely need fewer than " + (totalSquares-1) + " for a board that size, and probably more like " + (Math.floor(totalSquares/3)) + ".");
+		} else {
+			//first clear any previous boards
+			$("#board").html("");
+			// Cycles through each for the board height
+			for(i=0; i < boardHeight; i++){
+				thisRow = "";
+				for (var j = 0; j < boardWidth; j++) {
+					// builds up a row with the correct width
+					thisRow  += "<td id='s" + squareCount + "' class='box'></td>";
+					squareCount += 1;
+				};
+				// appends a new row with all that stuff in it for each of the height
+				$("#board").append( "<tr>" + thisRow + "</tr>" );
+				// $("#board").append( "Goodbye</tr>" );
 			};
-			// appends a new row with all that stuff in it for each of the height
-			$("#board").append( "<tr>" + thisRow + "</tr>" );
-			// $("#board").append( "Goodbye</tr>" );
-		};
+			placeMines(numMines, totalSquares);
+		}
 	});
 
 	var placeMines = function(numMines, height, width){
+		var totalSquares = height * width;
+
 
 
 	}
